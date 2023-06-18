9577b6ea83095486ee633b3cf38054fb6110b284
rotem <Rotem.ger207>
2023-06-18 16:32:13 +0300

    Fix type of date

--- src/utils/is-valid-date.js
+++ src/utils/is-valid-date.js
@@ -1,17 +1,19 @@
 function isValidDate(year, month, day) {
     // Define the number of days per month
     const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  
+
     // Leap years
-    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
-      daysInMonth[1] = 29;
+    if(typeof(month)!= "number" || typeof(year)!= "number" || typeof(day)!= "number" ){
+        return false;
+    }
+    else if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+        daysInMonth[1] = 29;
     }
     // Check if the date is valid
     if (month < 1 || month > 12 || day < 1 || day > daysInMonth[month - 1]) {
-      return false;
+        return false;
     }
     return true;
-  }
-  
-  module.exports = isValidDate;
-  
+}
+
+module.exports = isValidDate;
